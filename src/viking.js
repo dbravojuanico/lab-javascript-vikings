b0b9aa3745ad83d67ce31af14908fec1a943bd71
Dani Bravo <dbravojuanico@icloud.com>
2023-03-31 16:23:01 +0200

    Classes done, i dont know about the fight...

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,33 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor (health, strength) {
+    this.health = health;
+    this.strength = strength;
+    }
+    attack(){
+        return Soldier.strength
+    }
+    receiveDamage(the_damage){
+        return Soldier.health - 
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor (name, health, strength){
+        super (health, strength)
+        this.name = name
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {}
 
 // War
-class War {}
+class War {
+    addViking(){}
+    addSaxon(){}
+    vikingAttack(){}
+    saxonAttack(){}
+    showStatus(){}
+}
