fa542c7bfecf0370aff44604a71192b238bb1cc4
Dani Bravo <dbravojuanico@icloud.com>
2023-04-02 20:00:43 +0200

    missing the saxonAttack method

--- src/viking.js
+++ src/viking.js
@@ -44,10 +44,47 @@ class Saxon extends Soldier {
 }
 
 // War
+
 class War {
-    addViking(){}
-    addSaxon(){}
-    vikingAttack(){}
-    saxonAttack(){}
-    showStatus(){}
+    constructor (){
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+    addViking(a_Viking_object){
+        this.vikingArmy.push(a_Viking_object)
+    }
+    addSaxon(a_Saxon_object){
+        this.saxonArmy.push(a_Saxon_object)
+    }
+    vikingAttack(){
+        this.saxonArmy[0].health -= this.vikingArmy[0].strength
+        if (this.saxonArmy[0].health < 1){
+            this.saxonArmy.shift()
+        }
+        return 'A Saxon has died in combat'
+    }
+
+    saxonAttack(){
+        
+        for (let x=0; x < this.vikingArmy.length; x++){
+            this.vikingArmy[x].health -= this.saxonArmy[0].strength
+            if (this.vikingArmy[x].health < 1){
+                this.vikingArmy.shift()
+            }
+        }
+        
+        `${vikingArmy[0].name} has received ${saxonArmy[0].strength} points of damage`
+    }
+
+    showStatus(){
+        if (this.saxonArmy.length < 1) {
+            return "Vikings have won the war of the century!"
+        }
+        if (this.vikingArmy.length < 1){
+            return 'Saxons have fought for their lives and survived another day...'
+        } 
+        if (this.vikingArmy.length > 0 && this.saxonArmy.length > 0){
+            return "Vikings and Saxons are still in the thick of battle."
+        }
+    }
 }
